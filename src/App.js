2b0de3fc4d8cfc60b30cc9df08214bcabb403d98
Azeez <abdulazeezojo24@gmail.com>
2022-09-04 17:52:05 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 //pages
-import Home from "./Pages/Home";
-import Orbit from "./Pages/Orbit";
-import Textanim from "./Pages/Textanim";
+// import Home from "./Pages/Home";
+// import Orbit from "./Pages/Orbit";
+// import Textanim from "./Pages/Textanim";
 import Another3d from "./Pages/Another3d";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="home" element={<Home />} />
         <Route path="orbit" element={<Orbit />} />
-        <Route path="text" element={<Textanim />} />
-        <Route path="samurai" element={<Another3d />} />
+        <Route path="text" element={<Textanim />} /> */}
+        <Route path="/" element={<Another3d />} />
       </Routes>
     </div>
   );
